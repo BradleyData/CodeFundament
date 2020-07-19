a2ab04c1690847367ae4d8ecff5611a94601ccf1
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2020-07-19 17:34:23 -0400

    Add HTML reporting to jest-cucumber
    Issue #3

--- containers/development/api/cucumberJson2Html.js
+++
@@ -1,7 +0,0 @@
-let yachr = require("yachr")
-let reporter = new yachr.Reporter()
-
-reporter.generate({
-    jsonFile: 'app/output/acceptanceTests.json',
-    output: 'app/output/acceptanceTests.htm'
-})
