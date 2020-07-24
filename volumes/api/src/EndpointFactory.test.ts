0c93fd6c89a0ca125c48765c0fab26d5b4bf9ee9
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2020-07-24 15:56:20 -0400

    Test EndpointFactory
    Add all tests for EndpointFactory.
    
    Issue #3

---
+++ volumes/api/src/EndpointFactory.test.ts
@@ -0,0 +1,33 @@
+import EndpointFactory from "./EndpointFactory"
+import Endpoint from "./Endpoint"
+import DefaultEndpoint from "./endpoints/Default.v1"
+import InvalidEndpoint from "./endpoints/Invalid"
+import {mocked} from "ts-jest/utils"
+
+jest.mock("./Endpoint")
+jest.mock("./endpoints/Invalid")
+jest.mock("./endpoints/Default.v1")
+
+describe("EndpointFactory", () => {
+    test("with invalid instanceof", () => {
+        expect(() => {
+            EndpointFactory.createEndpoint("Default", 1, "", "")
+        }).toThrow("Invalid endpoint")
+    })
+
+    describe("with valid instanceof", () => {
+        const endpoint = mocked(Endpoint, true)
+
+        test("with valid version", () => {
+            const defaultEndpoint = mocked(DefaultEndpoint, true)
+            defaultEndpoint.prototype = endpoint.prototype
+            EndpointFactory.createEndpoint("Default", 1, "", "")
+        })
+
+        test("with invalid version", () => {
+            const invalidEndpoint = mocked(InvalidEndpoint, true)
+            invalidEndpoint.prototype = endpoint.prototype
+            EndpointFactory.createEndpoint("Default", -1, "", "")
+        })
+    })
+})
