59c007b7a504d29aaa1a29fb07fa432eaba686c9
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2020-07-27 12:30:10 -0400

    Linting

--- volumes/api/acceptanceTests/steps/endpointVersions.test.ts
+++ volumes/api/acceptanceTests/steps/endpointVersions.test.ts
@@ -1,22 +1,25 @@
+import { Then, When } from "cucumber"
 // eslint-disable-next-line no-unused-vars
 import Endpoint from "../../src/Endpoint"
-import expect from "expect"
 import Srvr from "../../src/Srvr"
-import { When, Then } from "cucumber"
+import expect from "expect"
 
 let endpoint: Endpoint
 
-When('{word} is attempted on {string}', (action: string, url: string) => {
+When("{word} is attempted on {string}", (action: string, url: string) => {
     const srvr = new Srvr()
     endpoint = srvr.createEndpoint(action, url)
 })
 
-Then("the endpoint {string} with version {int} is used", (name: string, version: number) => {
-    expect(name).toBe(endpoint.getName())
-    expect(version).toBe(endpoint.getVersion())
-})
+Then(
+    "the endpoint {string} with version {int} is used",
+    (name: string, version: number) => {
+        expect(name).toBe(endpoint.getName())
+        expect(version).toBe(endpoint.getVersion())
+    }
+)
 
-Then('is passed parameters {string}', (parameters: string) => {
+Then("is passed parameters {string}", (parameters: string) => {
     expect(parameters).toBe(endpoint.getParameters())
 })
 
@@ -28,6 +31,6 @@ Then("the number of rows affected is {int}", (rows: number) => {
     expect(rows).toBe(endpoint.getRowsAffected())
 })
 
-Then('the json output is {string}', (response: string) => {
+Then("the json output is {string}", (response: string) => {
     expect(response).toBe(endpoint.getResponse())
 })
