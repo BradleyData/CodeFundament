d3b7add610a2e3bb1ad3d1f5bd74780d465e2a34
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2020-09-08 17:37:30 -0400

    Add script to show all version lines
    Create container to run bash scripts in.
    
    Create bash script to find all lines with version numbers that may need
    updating.
    
    Create task to run the script.

---
+++ containers/support/bash/checkVersions.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+
+function printFile () {
+    echo -n $1 | sed "s|^framework/||"
+}
+
+function displayWithColumns () {
+    firstColumnWidth=$(cat $1 | cut -d$'\t' -f 1 | wc -L)
+    sort -t$'\t' -k 2,2 $1 | awk -F $'\t' '{ printf("%-'${firstColumnWidth}'s %s\n", $1, $2)}'
+    echo
+}
+
+function getVersionLines () {
+    cmd1="grep -h -E '$2' '$1'"
+
+    cmd2=""
+    if [ $# -eq 4 ]; then
+        cmd2="| grep -v '$4'"
+    fi
+
+    cmd3="| sed \"s|^|$(printFile $1):\t|\""
+
+    eval "$cmd1 $cmd2 $cmd3 >> $3"
+}
+
+echo
+echo "These docker-compose versions should all match:"
+for file in framework/**/docker-compose.yml; do
+    getVersionLines "$file" '^version: .[0-9.]+.$' compose1.txt
+done
+displayWithColumns compose1.txt
+
+echo "Update the following docker-compose images:"
+for file in framework/**/docker-compose.yml; do
+    getVersionLines "$file" 'image: .+:' compose2.txt
+done
+displayWithColumns compose2.txt
+
+echo "Update the following Dockerfile versions:"
+for file in framework/containers/**/Dockerfile; do
+    getVersionLines "$file" 'FROM .+:.*[0-9]' dockerfile.txt
+done
+displayWithColumns dockerfile.txt
+
+echo "Update the following package.json dependencies:"
+for file in framework/containers/**/package.json; do
+    getVersionLines "$file" '[0-9]+",?$' package.txt '"version": "[0-9]'
+done
+displayWithColumns package.txt
+echo
