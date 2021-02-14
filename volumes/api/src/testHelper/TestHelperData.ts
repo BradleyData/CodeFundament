b0aeff89031f3af89b97cb258789bb1c884e86ee
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2021-02-14 18:08:01 -0500

    Add random string generator

--- volumes/api/src/testHelper/TestHelperData.ts
+++ volumes/api/src/testHelper/TestHelperData.ts
@@ -1,7 +1,28 @@
 export class TestHelperData {
-    static randomInt = () => {
-        const maxRandomInt = 100
+    static randomInt(): number {
+        const max = 100
+        return this.randomIntWithMax({ max })
+    }
 
-        return Math.floor(Math.random() * maxRandomInt)
+    static randomIntWithMax({ max }: { max: number }): number {
+        return Math.floor(Math.random() * max)
+    }
+
+    static randomString(): string {
+        const length = 10
+
+        return [...Array(length)]
+            .map(() => {
+                const charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                const charLower = "abcdefghijklmnopqrstuvwxyz"
+                const charDigit = "0123456789"
+                const charOther = " '\"!@#$%^&*()-_=+~\\|;:{}[],.<>/?`"
+                const allChar = charUpper + charLower + charDigit + charOther
+
+                return allChar.charAt(
+                    this.randomIntWithMax({ max: allChar.length })
+                )
+            })
+            .join()
     }
 }
