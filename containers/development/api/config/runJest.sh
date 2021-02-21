1a56edcac7071172a14a7c1a78d913e9a0198ed7
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2021-02-21 13:18:27 -0500

    Remove jest open handles test
    Runs tests more slowly and has false positives with crypto library

--- containers/development/api/config/runJest.sh
+++ containers/development/api/config/runJest.sh
@@ -2,6 +2,6 @@
 
 cp /home/node/config/jest.config.js /home/node/
 cp /home/node/config/tsconfig.json /home/node/
-jest --detectOpenHandles $@
+jest $@
 rm -f /home/node/jest.config.js
 rm -f /home/node/tsconfig.json
