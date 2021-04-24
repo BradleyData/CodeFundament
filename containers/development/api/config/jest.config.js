b74b476eb935a69969ca7b843c62eefeb3401db4
ryan7273 <2078666+ryan7273@users.noreply.github.com>
2021-04-24 13:51:50 -0400

    issue54 (#55)
    * Update dependencies
    
    Remove unfinished code. Everything is now in a working state.
    
    * Update stryker
    
    Stryker changed the way it handles setting jest's rootDir. Change the
    jest config to use a relative path to fix the issue.

--- containers/development/api/config/jest.config.js
+++ containers/development/api/config/jest.config.js
@@ -117,7 +117,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: "/home/node/app/src",
+  rootDir: "./app/src",
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
--- volumes/api/acceptanceTests/features/account/creation.feature
+++
@@ -1,5 +0,0 @@
-Feature: A person can create a new account
-
-    Scenario: A person without an account
-        When a person attempts to create an account
-        Then an account is created
