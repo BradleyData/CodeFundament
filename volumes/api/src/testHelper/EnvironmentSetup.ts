354645da18764336767b37b4ae507d8614f7706e
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2022-01-14 17:10:17 -0500

    Update dependencies

--- volumes/api/src/testHelper/EnvironmentSetup.ts
+++ volumes/api/src/testHelper/EnvironmentSetup.ts
@@ -5,7 +5,7 @@ export class EnvironmentSetup {
     static getSuiteName({ __filename }: { __filename: string }): string {
         return __filename.replace(/.test.ts$/u, ".ts")
     }
-    
+
     static initSinonChai() {
         Chai.use(SinonChai)
     }
