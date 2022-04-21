d7ed6d5eeaec2500ab3d5f788c6bdbb9cdbe5901
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2022-04-21 13:56:59 -0400

    Add retrieveOriginalFile
    Add ability to get the parent of a feature branch.
    
    Add way to retrieve a file as it existed in the parent immediately prior
    to branch creation.

--- volumes/dbmigration/app/src/wrapperUnshared/Git.ts
+++ volumes/dbmigration/app/src/wrapperUnshared/Git.ts
@@ -5,7 +5,27 @@ export class Git {
     private static gitDir = `${Git.workTree}/.git`
     private static cmd = `git --work-tree=${Git.workTree} --git-dir=${Git.gitDir}`
 
+    static retrieveOriginalFile({from, to}: {from: string, to: string}): void {
+        const externalPath = 'volumes/dbmigration/migrations/'
+        const internalPath = '/home/node/migrations/'
+
+        const fullFrom = `${externalPath}${from}`
+        const fullTo = `${internalPath}${to}`
+
+        execSync(`${Git.cmd} show ${Git.getBranchParent()}:${fullFrom} 2>/dev/null > ${fullTo} || true`)
+    }
+
     static getBranchName(): string {
         return execSync(`${Git.cmd} rev-parse --abbrev-ref HEAD`).toString()
+    }
+
+    static getBranchParent(): string {
+        return execSync(`${Git.cmd} show-branch --merge-base ${Git.getDefaultBranch()}`).toString()
+    }
+
+    static getDefaultBranch(): string {
+        const path = 'refs/remotes/origin/'
+
+        return execSync(`${Git.cmd} symbolic-ref ${path}HEAD | sed 's@^${path}@@'`).toString()
     }
 }
