28054e667bc76a561f7ee46ee2e6c868aa50cfeb
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2022-04-26 11:14:02 -0400

    Refactor
    Add p-branch files to gitignore.
    
    Refactor branch retrieval code to remove duplication and improve
    readability.

--- volumes/dbmigration/app/src/wrapperUnshared/Git.ts
+++ volumes/dbmigration/app/src/wrapperUnshared/Git.ts
@@ -19,25 +19,38 @@ export class Git {
         return execSync(`${Git.cmd} symbolic-ref ${path}HEAD | sed 's@^${path}@@'`).toString()
     }
 
+    private static retrieveP({pType, branch}: {pType: string, branch: string}): void {
+        updateGit()
+        addTree()
+        retrieveFiles()
+        removeTree()
+
+        function updateGit(): void {
+            execSync(`${Git.cmd} fetch --all`)
+        }
+
+        function addTree(): void {
+            removeTree()
+            execSync(`${Git.cmd} worktree add /home/node/gitRepos/${pType} ${branch}`)
+        }
+
+        function removeTree(): void {
+            execSync(`${Git.cmd} worktree remove --force /home/node/gitRepos/${pType} 2>/dev/null || true`)
+        }
+
+        function retrieveFiles(): void {
+            execSync(`rm -rf /home/node/volumes/api${pType}/*`)
+            execSync(`mv /home/node/gitRepos/${pType}/volumes/api/* /home/node/volumes/api${pType}/`)
+            execSync(`touch /home/node/volumes/api${pType}/src/.gitkeep`)
+            execSync(`mv /home/node/gitRepos/${pType}/volumes/dbmigration/migrations/schema.sql /home/node/migrations/schema${pType}.sql || touch /home/node/migrations/schema${pType}.sql`)
+        }
+    }
+
     static retrievePrime(): void {
-        execSync(`${Git.cmd} fetch --all`)
-        execSync(`${Git.cmd} worktree remove --force /home/node/gitRepos/prime 2>/dev/null || true`)
-        execSync(`${Git.cmd} worktree add /home/node/gitRepos/prime ${Git.getBranchParent()}`)
-        execSync(`rm -rf /home/node/volumes/apiprime/*`)
-        execSync(`mv /home/node/gitRepos/prime/volumes/api/* /home/node/volumes/apiprime/`)
-        execSync(`touch /home/node/volumes/apiprime/src/.gitkeep`)
-        execSync(`mv /home/node/gitRepos/prime/volumes/dbmigration/migrations/schema.sql /home/node/migrations/schemaPrime.sql || touch /home/node/migrations/schemaPrime.sql`)
-        execSync(`${Git.cmd} worktree remove --force /home/node/gitRepos/prime 2>/dev/null || true`)
+        Git.retrieveP({pType: 'prime', branch: Git.getBranchParent()})
     }
 
     static retrieveProduction(): void {
-        execSync(`${Git.cmd} fetch --all`)
-        execSync(`${Git.cmd} worktree remove --force /home/node/gitRepos/production 2>/dev/null || true`)
-        execSync(`${Git.cmd} worktree add /home/node/gitRepos/production ${Git.getBranchParent()}`)
-        execSync(`rm -rf /home/node/volumes/apiproduction/*`)
-        execSync(`mv /home/node/gitRepos/production/volumes/api/* /home/node/volumes/apiproduction/`)
-        execSync(`touch /home/node/volumes/apiproduction/src/.gitkeep`)
-        execSync(`mv /home/node/gitRepos/production/volumes/dbmigration/migrations/schema.sql /home/node/migrations/schemaProduction.sql || touch /home/node/migrations/schemaProduction.sql`)
-        execSync(`${Git.cmd} worktree remove --force /home/node/gitRepos/production 2>/dev/null || true`)
+        Git.retrieveP({pType: 'production', branch: Git.getDefaultBranch()})
     }
 }
