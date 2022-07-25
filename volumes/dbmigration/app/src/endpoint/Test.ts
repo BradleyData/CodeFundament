361bebd1ebad7f490300882139a698c80f6504d2
Ryan Bradley <2078666+ryan7273@users.noreply.github.com>
2022-07-25 10:37:17 -0400

    Add endpoint to run branch tests

---
+++ volumes/dbmigration/app/src/endpoint/Test.ts
@@ -0,0 +1,19 @@
+import { Branch } from "../Branch"
+import { Endpoint } from "../Endpoint"
+import { Git } from "../wrapperUnshared/Git"
+
+class Test extends Endpoint {
+    protected async get(): Promise<void> {
+        const branchType: Git.branchType | "" = Git.branchType[this.parameters["branchType"] as Git.branchType] ?? ""
+        const content = await Branch.runTests({branchType})
+        this.response = `<html><body>${contentToString()}</body></html>`
+
+        function contentToString(): string {
+            return content.reduce((current, addend) => {
+                return `${current}<br>${addend}`
+            })
+        }
+    }
+}
+
+export { Test, Test as Endpoint }
